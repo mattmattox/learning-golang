5a517631f821cbe52b268ba32333462d7183f28e
Matthew Mattox <mmattox@support.tools>
2021-02-07 01:27:06 -0600

    renaming dirs

---
+++ 03-importing/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	testfmt "fmt"
+)
+
+func main() {
+	fmt.Println("Hello!")
+	testfmt.Println("There!")
+	bye()
+	hey()
+}
---
+++ 04-statements/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello!")
+
+	if 5 > 1 {
+		fmt.Println("bigger")
+	}
+}
